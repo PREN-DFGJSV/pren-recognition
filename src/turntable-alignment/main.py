88988237120970a0806f029e2860381a47e42ffa
Gian Ott <nichtgian@gmail.com>
2023-10-26 11:07:08 +0200

    Add sample scene renderings

---
+++ src/turntable-alignment/main.py
@@ -0,0 +1,19 @@
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(sys.path[0]), "common"))
+
+from TurntableQuadrant import TurntableQuadrant
+
+import cv2
+
+# TODO: Remove this temporary main
+turntable = TurntableQuadrant("res/XGGR_XXRX.mp4")
+turntable.frame_when_aligned(0, 90)
+cv2.waitKey(0)
+
+turntable.frame_when_aligned(46 + 80, 20)
+
+cv2.waitKey(0)
+turntable.frame_when_aligned(46 + 80 + 90, 20)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
