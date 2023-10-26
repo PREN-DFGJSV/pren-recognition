d7d3bb56163eabd09c1484fb7b0327550d566128
Dario Gamma <dario.gamma@outlook.com>
2023-10-27 00:02:28 +0200

    Kommentar korrigiert

--- src/enums/EOrientierung.py
+++ src/enums/EOrientierung.py
@@ -2,7 +2,7 @@ from enum import Enum
 
 
 class EOrientierung(Enum):
-    NORD = 1  # Quadrant oben Links
-    OST = 2  # Quadrant oben Rechts
+    NORD = 1  # Quadrant oben Rechts
+    OST = 2  # Quadrant oben Links
     SUED = 3  # Quadrant unten Links
     WEST = 4  # Quadrant unten Rechts
