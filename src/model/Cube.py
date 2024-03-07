4f61f21ca35e5e954da8177178bc24716dcede69
Dario Gamma <dario.gamma@outlook.com>
2024-03-07 10:04:17 +0100

    Kleiner Fix

--- src/model/Cube.py
+++ src/model/Cube.py
@@ -4,7 +4,7 @@ import string
 
 class Cube:
     def __int__(self, pos1: string = '', pos2: string = '', pos3: string = '', pos4: string = '', pos5: string = '',
-                pos6: string = '', pos7: string = ''):
+                pos6: string = '', pos7: string = '', pos8: string = ''):
         self.pos1: string = pos1
         self.pos2: string = pos2
         self.pos3: string = pos3
@@ -12,6 +12,7 @@ class Cube:
         self.pos5: string = pos5
         self.pos6: string = pos6
         self.pos7: string = pos7
+        self.pos8: string = pos8
 
     def to_json(self):
         return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
