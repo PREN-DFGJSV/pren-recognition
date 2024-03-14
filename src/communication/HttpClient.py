5686b9488abcbee23c14f38bab77df3a26b936e2
Dario Gamma <dario.gamma@outlook.com>
2024-03-14 09:54:43 +0100

    DB und HttpClient ergänzt

--- src/communication/HttpClient.py
+++ src/communication/HttpClient.py
@@ -4,10 +4,10 @@ from src.model.BaseDto import BaseDto
 
 
 class HttpClient:
-    def __init__(self, url: string):
-        # TODO: URL Validieren
+    def __init__(self, url: string, auth: string):
         self.url = url
+        self.auth = auth
 
     def post_dto(self, dto: BaseDto) -> string:
-        result = requests.post(url=self.url, json=dto.to_json())
+        result = requests.post(url=self.url, auth=self.auth, json=dto.to_json())
         return result.status_code
